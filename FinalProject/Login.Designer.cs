a058d1d8cdcf1409634130e463ef204ac722a948
Tran Luan <tranluanqqq@gmail.com>
2020-12-03 16:52:56 +0700

    checkmaster

--- FinalProject/Login.Designer.cs
+++ FinalProject/Login.Designer.cs
@@ -32,7 +32,6 @@
             this.tbTaiKhoan = new System.Windows.Forms.TextBox();
             this.tbMatKhau = new System.Windows.Forms.TextBox();
             this.btnDangNhap = new System.Windows.Forms.Button();
-            this.btnDangKy = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
@@ -49,9 +48,10 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Bell MT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(214)))));
-            this.label2.Location = new System.Drawing.Point(36, 124);
+            this.label2.Location = new System.Drawing.Point(48, 153);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(423, 25);
+            this.label2.Size = new System.Drawing.Size(539, 31);
             this.label2.TabIndex = 1;
             this.label2.Text = "RESTAURANT MANAGEMENT SYSTEM";
             // 
@@ -60,9 +60,10 @@
             this.tbTaiKhoan.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.tbTaiKhoan.Font = new System.Drawing.Font("Bell MT", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tbTaiKhoan.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(214)))));
-            this.tbTaiKhoan.Location = new System.Drawing.Point(158, 256);
+            this.tbTaiKhoan.Location = new System.Drawing.Point(211, 315);
+            this.tbTaiKhoan.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tbTaiKhoan.Name = "tbTaiKhoan";
-            this.tbTaiKhoan.Size = new System.Drawing.Size(213, 17);
+            this.tbTaiKhoan.Size = new System.Drawing.Size(284, 22);
             this.tbTaiKhoan.TabIndex = 3;
             this.tbTaiKhoan.TextChanged += new System.EventHandler(this.tbTaiKhoan_TextChanged);
             // 
@@ -71,9 +72,10 @@
             this.tbMatKhau.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.tbMatKhau.Font = new System.Drawing.Font("Bell MT", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tbMatKhau.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(214)))));
-            this.tbMatKhau.Location = new System.Drawing.Point(158, 332);
+            this.tbMatKhau.Location = new System.Drawing.Point(211, 409);
+            this.tbMatKhau.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tbMatKhau.Name = "tbMatKhau";
-            this.tbMatKhau.Size = new System.Drawing.Size(213, 17);
+            this.tbMatKhau.Size = new System.Drawing.Size(284, 22);
             this.tbMatKhau.TabIndex = 4;
             // 
             // btnDangNhap
@@ -82,60 +84,52 @@
             this.btnDangNhap.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnDangNhap.Font = new System.Drawing.Font("Bell MT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnDangNhap.ForeColor = System.Drawing.Color.White;
-            this.btnDangNhap.Location = new System.Drawing.Point(121, 377);
+            this.btnDangNhap.Location = new System.Drawing.Point(161, 470);
+            this.btnDangNhap.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnDangNhap.Name = "btnDangNhap";
-            this.btnDangNhap.Size = new System.Drawing.Size(250, 33);
+            this.btnDangNhap.Size = new System.Drawing.Size(333, 41);
             this.btnDangNhap.TabIndex = 5;
             this.btnDangNhap.Text = "LOG IN";
             this.btnDangNhap.UseVisualStyleBackColor = false;
             this.btnDangNhap.Click += new System.EventHandler(this.btnDangNhap_Click);
-            // 
-            // btnDangKy
-            // 
-            this.btnDangKy.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(214)))));
-            this.btnDangKy.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnDangKy.Font = new System.Drawing.Font("Bell MT", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDangKy.ForeColor = System.Drawing.Color.White;
-            this.btnDangKy.Location = new System.Drawing.Point(121, 426);
-            this.btnDangKy.Name = "btnDangKy";
-            this.btnDangKy.Size = new System.Drawing.Size(250, 33);
-            this.btnDangKy.TabIndex = 6;
-            this.btnDangKy.Text = "REGISTER";
-            this.btnDangKy.UseVisualStyleBackColor = false;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(214)))));
-            this.label4.Location = new System.Drawing.Point(196, 170);
+            this.label4.Location = new System.Drawing.Point(261, 209);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(81, 29);
+            this.label4.Size = new System.Drawing.Size(103, 37);
             this.label4.TabIndex = 8;
             this.label4.Text = "LOG IN";
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(214)))));
-            this.panel1.Location = new System.Drawing.Point(121, 282);
+            this.panel1.Location = new System.Drawing.Point(161, 347);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(250, 1);
+            this.panel1.Size = new System.Drawing.Size(333, 1);
             this.panel1.TabIndex = 10;
             // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(214)))));
-            this.panel2.Location = new System.Drawing.Point(121, 358);
+            this.panel2.Location = new System.Drawing.Point(161, 441);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(250, 1);
+            this.panel2.Size = new System.Drawing.Size(333, 1);
             this.panel2.TabIndex = 10;
             // 
             // pictureBox3
             // 
             this.pictureBox3.Image = global::FinalProject.Properties.Resources.kisspng_computer_icons_password_portable_network_graphics_user_dashboard_5c57ecf8d61433_6078174815492661688769;
-            this.pictureBox3.Location = new System.Drawing.Point(121, 320);
+            this.pictureBox3.Location = new System.Drawing.Point(161, 394);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(31, 32);
+            this.pictureBox3.Size = new System.Drawing.Size(41, 39);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox3.TabIndex = 9;
             this.pictureBox3.TabStop = false;
@@ -143,9 +137,10 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = global::FinalProject.Properties.Resources.user_icon_png_pnglogocom_133466;
-            this.pictureBox2.Location = new System.Drawing.Point(121, 244);
+            this.pictureBox2.Location = new System.Drawing.Point(161, 300);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(31, 32);
+            this.pictureBox2.Size = new System.Drawing.Size(41, 39);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 9;
             this.pictureBox2.TabStop = false;
@@ -153,31 +148,32 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::FinalProject.Properties.Resources.LOGOTDTU;
-            this.pictureBox1.Location = new System.Drawing.Point(176, 45);
+            this.pictureBox1.Location = new System.Drawing.Point(235, 55);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(123, 65);
+            this.pictureBox1.Size = new System.Drawing.Size(164, 80);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 7;
             this.pictureBox1.TabStop = false;
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(491, 544);
+            this.ClientSize = new System.Drawing.Size(655, 670);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.btnDangKy);
             this.Controls.Add(this.btnDangNhap);
             this.Controls.Add(this.tbMatKhau);
             this.Controls.Add(this.tbTaiKhoan);
             this.Controls.Add(this.label2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "Login";
             this.Text = "Form1";
@@ -194,7 +190,6 @@
         private System.Windows.Forms.TextBox tbTaiKhoan;
         private System.Windows.Forms.TextBox tbMatKhau;
         private System.Windows.Forms.Button btnDangNhap;
-        private System.Windows.Forms.Button btnDangKy;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.PictureBox pictureBox2;
