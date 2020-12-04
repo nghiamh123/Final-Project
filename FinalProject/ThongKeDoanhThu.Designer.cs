bad8f6672f379420213b984eb10c88e692596146
Tran Luan <tranluanqqq@gmail.com>
2020-12-04 21:06:30 +0700

    Ngia updategiaodienThongKe

--- FinalProject/ThongKeDoanhThu.Designer.cs
+++ FinalProject/ThongKeDoanhThu.Designer.cs
@@ -42,125 +42,226 @@ namespace FinalProject
             this.label5 = new System.Windows.Forms.Label();
             this.tbDoanhThuDon = new System.Windows.Forms.TextBox();
             this.btnReset = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.dtgBill)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // dtFrom
             // 
-            this.dtFrom.Location = new System.Drawing.Point(98, 44);
+            this.dtFrom.Font = new System.Drawing.Font("Bell MT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtFrom.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtFrom.Location = new System.Drawing.Point(68, 81);
+            this.dtFrom.Margin = new System.Windows.Forms.Padding(2);
             this.dtFrom.Name = "dtFrom";
-            this.dtFrom.Size = new System.Drawing.Size(200, 22);
+            this.dtFrom.Size = new System.Drawing.Size(151, 22);
             this.dtFrom.TabIndex = 0;
             // 
             // dtTo
             // 
-            this.dtTo.Location = new System.Drawing.Point(433, 44);
+            this.dtTo.Font = new System.Drawing.Font("Bell MT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtTo.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtTo.Location = new System.Drawing.Point(295, 81);
+            this.dtTo.Margin = new System.Windows.Forms.Padding(2);
             this.dtTo.Name = "dtTo";
-            this.dtTo.Size = new System.Drawing.Size(200, 22);
+            this.dtTo.Size = new System.Drawing.Size(151, 22);
             this.dtTo.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(35, 49);
+            this.label1.Font = new System.Drawing.Font("Bell MT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(214)))));
+            this.label1.Location = new System.Drawing.Point(8, 82);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(40, 17);
+            this.label1.Size = new System.Drawing.Size(56, 24);
             this.label1.TabIndex = 2;
             this.label1.Text = "From";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(367, 44);
+            this.label2.Font = new System.Drawing.Font("Bell MT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(214)))));
+            this.label2.Location = new System.Drawing.Point(257, 82);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(25, 17);
+            this.label2.Size = new System.Drawing.Size(34, 24);
             this.label2.TabIndex = 3;
             this.label2.Text = "To";
             // 
             // dtgBill
             // 
             this.dtgBill.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dtgBill.Location = new System.Drawing.Point(98, 87);
+            this.dtgBill.Location = new System.Drawing.Point(11, 113);
+            this.dtgBill.Margin = new System.Windows.Forms.Padding(2);
             this.dtgBill.Name = "dtgBill";
             this.dtgBill.RowHeadersWidth = 51;
             this.dtgBill.RowTemplate.Height = 24;
-            this.dtgBill.Size = new System.Drawing.Size(640, 269);
+            this.dtgBill.Size = new System.Drawing.Size(447, 226);
             this.dtgBill.TabIndex = 4;
             this.dtgBill.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgBill_CellClick);
             // 
             // tbTongDoanhThu
             // 
-            this.tbTongDoanhThu.Location = new System.Drawing.Point(370, 457);
+            this.tbTongDoanhThu.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbTongDoanhThu.Font = new System.Drawing.Font("Bell MT", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbTongDoanhThu.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(214)))));
+            this.tbTongDoanhThu.Location = new System.Drawing.Point(572, 174);
+            this.tbTongDoanhThu.Margin = new System.Windows.Forms.Padding(2);
             this.tbTongDoanhThu.Name = "tbTongDoanhThu";
-            this.tbTongDoanhThu.Size = new System.Drawing.Size(154, 22);
+            this.tbTongDoanhThu.Size = new System.Drawing.Size(93, 13);
             this.tbTongDoanhThu.TabIndex = 5;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(225, 462);
+            this.label3.Font = new System.Drawing.Font("Bell MT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(214)))));
+            this.label3.Location = new System.Drawing.Point(462, 177);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(109, 17);
+            this.label3.Size = new System.Drawing.Size(89, 17);
             this.label3.TabIndex = 6;
-            this.label3.Text = "Tổng doanh thu";
+            this.label3.Text = "Total Revenue";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(128, 397);
+            this.label4.Font = new System.Drawing.Font("Bell MT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(214)))));
+            this.label4.Location = new System.Drawing.Point(462, 113);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(62, 17);
+            this.label4.Size = new System.Drawing.Size(83, 17);
             this.label4.TabIndex = 7;
-            this.label4.Text = "Tỏng bill";
+            this.label4.Text = "Total Invoice";
             // 
             // tbTongBill
             // 
-            this.tbTongBill.Location = new System.Drawing.Point(228, 390);
+            this.tbTongBill.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbTongBill.Font = new System.Drawing.Font("Bell MT", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbTongBill.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(214)))));
+            this.tbTongBill.Location = new System.Drawing.Point(573, 110);
+            this.tbTongBill.Margin = new System.Windows.Forms.Padding(2);
             this.tbTongBill.Name = "tbTongBill";
-            this.tbTongBill.Size = new System.Drawing.Size(100, 22);
+            this.tbTongBill.Size = new System.Drawing.Size(93, 13);
             this.tbTongBill.TabIndex = 8;
             // 
             // btnCheck
             // 
-            this.btnCheck.Location = new System.Drawing.Point(767, 241);
+            this.btnCheck.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(214)))));
+            this.btnCheck.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCheck.Font = new System.Drawing.Font("Bell MT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCheck.ForeColor = System.Drawing.Color.White;
+            this.btnCheck.Location = new System.Drawing.Point(574, 305);
+            this.btnCheck.Margin = new System.Windows.Forms.Padding(2);
             this.btnCheck.Name = "btnCheck";
-            this.btnCheck.Size = new System.Drawing.Size(75, 23);
+            this.btnCheck.Size = new System.Drawing.Size(92, 34);
             this.btnCheck.TabIndex = 9;
-            this.btnCheck.Text = "Lọc";
-            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Text = "FILTER";
+            this.btnCheck.UseVisualStyleBackColor = false;
             this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(367, 393);
+            this.label5.Font = new System.Drawing.Font("Bell MT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(214)))));
+            this.label5.Location = new System.Drawing.Point(462, 145);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(184, 17);
+            this.label5.Size = new System.Drawing.Size(102, 17);
             this.label5.TabIndex = 10;
-            this.label5.Text = "Doanh Thu Từng Đơn Hàng";
+            this.label5.Text = "Sales Per Invoice";
             // 
             // tbDoanhThuDon
             // 
-            this.tbDoanhThuDon.Location = new System.Drawing.Point(576, 388);
+            this.tbDoanhThuDon.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbDoanhThuDon.Font = new System.Drawing.Font("Bell MT", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbDoanhThuDon.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(214)))));
+            this.tbDoanhThuDon.Location = new System.Drawing.Point(573, 141);
+            this.tbDoanhThuDon.Margin = new System.Windows.Forms.Padding(2);
             this.tbDoanhThuDon.Name = "tbDoanhThuDon";
-            this.tbDoanhThuDon.Size = new System.Drawing.Size(123, 22);
+            this.tbDoanhThuDon.Size = new System.Drawing.Size(93, 13);
             this.tbDoanhThuDon.TabIndex = 11;
             // 
             // btnReset
             // 
-            this.btnReset.Location = new System.Drawing.Point(767, 166);
+            this.btnReset.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(214)))));
+            this.btnReset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReset.Font = new System.Drawing.Font("Bell MT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.ForeColor = System.Drawing.Color.White;
+            this.btnReset.Location = new System.Drawing.Point(465, 305);
+            this.btnReset.Margin = new System.Windows.Forms.Padding(2);
             this.btnReset.Name = "btnReset";
-            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.Size = new System.Drawing.Size(92, 34);
             this.btnReset.TabIndex = 12;
-            this.btnReset.Text = "Reset";
-            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Text = "RESET";
+            this.btnReset.UseVisualStyleBackColor = false;
             this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Bell MT", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(214)))));
+            this.label6.Location = new System.Drawing.Point(224, 27);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(310, 31);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "STATISTICAL TABLES";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(214)))));
+            this.panel1.Location = new System.Drawing.Point(465, 129);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 1);
+            this.panel1.TabIndex = 14;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(214)))));
+            this.panel2.Location = new System.Drawing.Point(465, 161);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 1);
+            this.panel2.TabIndex = 15;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(214)))));
+            this.panel3.Location = new System.Drawing.Point(465, 193);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 1);
+            this.panel3.TabIndex = 16;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::FinalProject.Properties.Resources.LOGOTDTU;
+            this.pictureBox1.Location = new System.Drawing.Point(490, 217);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(137, 83);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
             // 
             // ThongKeDoanhThu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(875, 495);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(693, 364);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.btnReset);
             this.Controls.Add(this.tbDoanhThuDon);
             this.Controls.Add(this.label5);
@@ -174,9 +275,13 @@ namespace FinalProject
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dtTo);
             this.Controls.Add(this.dtFrom);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MinimizeBox = false;
             this.Name = "ThongKeDoanhThu";
             this.Text = "ThongKeDoanhThu";
             ((System.ComponentModel.ISupportInitialize)(this.dtgBill)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -197,5 +302,10 @@ namespace FinalProject
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox tbDoanhThuDon;
         private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
